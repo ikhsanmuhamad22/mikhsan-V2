b3830a209b6411dd8b6fcc38e9c009fca511c852
ikhsanmuhamad22 <ikhsanmuhamad3885@gmail.com>
2023-11-27 11:56:19 +0700

    add section contacts

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,10 +1,12 @@
 import HeaderFixed from './header/HeaderFixed';
 import SectionAbout from './section/SectionAbout';
+import SectionContacts from './section/SectionCotacts';
 function App() {
   return (
     <>
       <HeaderFixed />
       <SectionAbout />
+      <SectionContacts />
     </>
   );
 }
