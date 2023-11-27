03c208e25539f2d179e543f6c45132f5abe9f320
ikhsanmuhamad22 <ikhsanmuhamad3885@gmail.com>
2023-11-27 07:22:42 +0700

    update Header Components

--- src/Components/header/HeaderFixed.tsx
+++ src/Components/header/HeaderFixed.tsx
@@ -1,43 +1,21 @@
+import { Nav } from 'react-bootstrap';
+
 const HeaderFixed = () => {
   return (
-    <nav id="navbar" className="navbar navbar-expand-lg">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <div className="d-flex">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  About
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  Works
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </nav>
+    <Nav className="navbar">
+      <a href=""></a>
+      <ul className="nav-list">
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Works</a>
+        </li>
+        <li>
+          <a href="#">Contacts</a>
+        </li>
+      </ul>
+    </Nav>
   );
 };
 
